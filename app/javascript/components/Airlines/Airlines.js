8aad05941421c5b54b8fa740cf9c48e43a634bc0
Sick-Carlito <carlb1319@gmail.com>
2022-02-03 00:51:48 +0300

    Create Airline component within Airlines folder and style usong styled components

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,37 +1,63 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Airline from '../Airline/Airline';
+import Airline from "./Airline";
+import styled from 'styled-components'
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/v1/airlines.json')
-     .then( resp => setAirlines(resp.data.data))
-    .catch( data => {
-      debugger
-    })
-  },[])
+    axios
+      .get("/api/v1/airlines.json")
+      .then((resp) => setAirlines(resp.data.data))
+      .catch((resp) => console.log(resp));
+  }, []);
 
-    const grid = airlines.map( (airline, index) => {
-      return (<li key={index} >{airline.attributes.name}</li>)
-  })
+  const grid = airlines.map((airline, index) => {
+    return <Airline key={index} attributes={airline.attributes} />;
+  });
+
+  const Home = styled.div`
+    text-align:center;
+  `
+  const Header = styled.div`
+    padding:100px 100px 10px 100px;
+    
+    h1 {
+      font-size:42px;
+    
+  `
+
+  const Subheader = styled.p`
+   font-weight:300;
+   font-size:26px;
+  `
+
+  const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    padding:20px;
+  `
+
+
+
+
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <p className="subheader">
+        <Subheader>
           Honest, unbiased airline reviews. Share your experience.
-        </p>
-      </div>
-      <div className="grid">
-        <ul>
-          {grid}
-        </ul>
-      </div>
-    </div>
+        </Subheader>
+      </Header>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
   );
 };
 
