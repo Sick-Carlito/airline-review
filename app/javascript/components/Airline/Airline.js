d565b06416e40cbbfa99e4558a97b81f910f0aa5
Sick-Carlito <carlb1319@gmail.com>
2022-02-07 23:05:16 +0300

    Modified Airline component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Airline = () => {
+
+
+const Airline = (props) => {
+  
+//  const slug = props.match.params.slug
+
+//  console.log(slug)
+
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+
+
+
+   useEffect( () => {
+    const slug = props.match.params.slug
+    const url = '/api/v1/airlines/' + slug
+
+    
+
+    axios.get(url)
+    .then( (resp) => {
+      console.log('data', resp)
+    })
+    .catch( data => {
+      debugger
+    })
+    
+  }, [])
+
   return(
     <div>This is the Airlines#show page for our app.</div>
   )
 }
 
-export default Airline
+export default Airline;
--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,27 +1,60 @@
-
-import React from 'react'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Airline = (props) => {
-  const {name, image_url, slug} = props.attributes
+  const { name, image_url, slug } = props.attributes;
+
+  // console.log(props.attributes)
+
+  const Card = styled.div`
+    border: 1px solid #efefef;
+    background: #fff;
+    font-family: sans-serif;
+    padding: 10px;
+  `;
+
+  const AirlineLogo = styled.div`
+    height: 50px;
+  `;
+
+  const AirlineName = styled.div`
+    padding: 20px;
+  `;
+
+  const LinkWrapper = styled.div`
+    margin: 20px 0;
+
+    a {
+      color: #fff;
+      background-color: #71b406;
+      border-radius: 4px;
+      padding: 10px 30px;
+      cursor: pointer;
+      border-radius: 3px;
+      border: 1px solid #71b406;
+      text-align: center;
+      line-height: 20px;
+      min-height: 40px;
+      margin: 7px;
+      font-weight: 600;
+      text-decoration: none;
+    }
+  `;
 
   return (
-    <div className="card">
-
-      <div className="airline-logo">
+    <Card>
+      <AirlineLogo>
         <img src={image_url} alt={name} width="50" />
-      </div>
+      </AirlineLogo>
 
-      <div className="airline-name">
-        {name}
-      </div>
+      <AirlineName> {name} </AirlineName>
 
-      <div className="link-wrapper">
-        <Link to={"/" + slug}>View Airline</Link>
-      </div>
+      <LinkWrapper>
+        <Link to={"/" + slug}> View Airline </Link>{" "}
+      </LinkWrapper>
+    </Card>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Airline
+export default Airline;
