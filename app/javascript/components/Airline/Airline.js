5bbc498b44e8e2015d5e708949eedbd5c9e66884
Sick-Carlito <carlb1319@gmail.com>
2022-02-08 23:49:33 +0300

    add header component and modified Airline compoent

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,39 +1,47 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from './Header'
 
 
 const Airline = (props) => {
-  
-//  const slug = props.match.params.slug
+  //  const slug = props.match.params.slug
 
-//  console.log(slug)
+  //  console.log(slug)
 
-  const [airline, setAirline] = useState({})
-  const [review, setReview] = useState({})
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+  const [loaded, setLoaded] = useState(false)
 
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    const url = "/api/v1/airlines/" + slug;
 
+    axios
+      .get(url)
+      .then((resp) => {
+        setAirline(resp.data);
+        console.log(resp.data)
+        setLoaded(true)
+      })
+      .catch( resp => console.log(resp) )
+  }, []);
 
-   useEffect( () => {
-    const slug = props.match.params.slug
-    const url = '/api/v1/airlines/' + slug
+  return (
+    <div>
+      <div className="column">
+        {
+          loaded &&
+          <Header attributes={airline.data.attributes}l/>
+        }
 
-    
-
-    axios.get(url)
-    .then( (resp) => {
-      console.log('data', resp)
-    })
-    .catch( data => {
-      debugger
-    })
-    
-  }, [])
-
-  return(
-    <div>This is the Airlines#show page for our app.</div>
-  )
-}
+        <div className="reviews">[reviews will go here]</div>
+      </div>
+      <div className="column">
+        [new review form will go here]
+      </div>
+    </div>
+  );
+};
 
 export default Airline;
