55bebf5ff8f2a02cf37ae601118783e16b827244
Sick-Carlito <carlb1319@gmail.com>
2022-02-15 15:34:20 +0300

    Build the average component and add it as one of the attributes to API

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Rating from '../Rating/Rating'
 
 const Header = (props) => {
-  const { name, image_url} = props.attributes;
+  const { name, image_url, avg_score} = props.attributes;
   // const name = props.match.params.name
   // const image_url = props.image_url
   return(
     <div className="header">
       <img src={image_url} alt={name} width="50"/>
       <h1>{name}</h1>
+      <Rating score={avg_score}/>
     </div>
   )
 }
